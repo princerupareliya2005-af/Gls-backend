dc42e6fc716839f356a25b907b0e614a2a4e2f2c
princerupareliya2005-af <princerupareliya2005@gmail.com>
2026-07-11 13:58:06 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+let express = require('express');
+let cors=require('cors');
+let app = express();
+let port = 9000;
+
+app.use(cors());
+app.get('/ping', (req, res) => {
+    res.send({
+        message: 'hello world'
+    });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}/ping`);
+});
